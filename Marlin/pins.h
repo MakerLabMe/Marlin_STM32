7419d8a6b834b9b89fb0af38b07cf805254f7a2a
AndySze <andy.sze.mail@gmail.com>
2015-01-15 23:54:14 +0800

    change pin maps for STduino Sun R1 and Ramps1.4

--- Marlin/pins.h
+++ Marlin/pins.h
@@ -2898,51 +2898,60 @@ DaveX plan for Teensylu/printrboard-type pinouts (ref teensylu & sprinter) for a
 
 #define LARGE_FLASH true
 
-#define X_STEP_PIN 25
-#define X_DIR_PIN 23
-#define X_MIN_PIN 22
-#define X_MAX_PIN 24
-#define X_ENABLE_PIN 27
+#define X_STEP_PIN         54
+#define X_DIR_PIN          57
+#define X_ENABLE_PIN       38
+#define X_MIN_PIN           3
+#define X_MAX_PIN           2
 
-#define Y_STEP_PIN 31
-#define Y_DIR_PIN 33
-#define Y_MIN_PIN 26
-#define Y_MAX_PIN 28
-#define Y_ENABLE_PIN 29
+#define Y_STEP_PIN         60
+#define Y_DIR_PIN          61
+#define Y_ENABLE_PIN       56
+#define Y_MIN_PIN          14
+#define Y_MAX_PIN          15
 
-#define Z_STEP_PIN 37
-#define Z_DIR_PIN 39
-#define Z_MIN_PIN 30
-#define Z_MAX_PIN 32
-#define Z_ENABLE_PIN 35
+#define Z_STEP_PIN         46
+#define Z_DIR_PIN          48
+#define Z_ENABLE_PIN       62
+#define Z_MIN_PIN          18
+#define Z_MAX_PIN          19
+
+#define Y2_STEP_PIN        36
+#define Y2_DIR_PIN         34
+#define Y2_ENABLE_PIN      30
+
+#define Z2_STEP_PIN        36
+#define Z2_DIR_PIN         34
+#define Z2_ENABLE_PIN      30
+
+#define E0_STEP_PIN        26
+#define E0_DIR_PIN         28
+#define E0_ENABLE_PIN      24
+
+#define E1_STEP_PIN        36
+#define E1_DIR_PIN         34
+#define E1_ENABLE_PIN      30
 
 #define HEATER_BED_PIN 4
 #define TEMP_BED_PIN 10
 
-#define HEATER_0_PIN  2
+#define HEATER_0_PIN  10
 #define TEMP_0_PIN A1//55
 
-#define HEATER_1_PIN 3
-#define TEMP_1_PIN A2//56
+#define HEATER_1_PIN -1//3
+#define TEMP_1_PIN -1//A2//56
 
 #define HEATER_2_PIN -1
 #define TEMP_2_PIN -1
 
-#define E0_STEP_PIN         43
-#define E0_DIR_PIN          45
-#define E0_ENABLE_PIN       41
-
-#define E1_STEP_PIN         49
-#define E1_DIR_PIN          47
-#define E1_ENABLE_PIN       48
 
 #define SDPOWER            -1
-#define SDSS               53
-#define LED_PIN            -1//13
-#define FAN_PIN            7
-#define PS_ON_PIN          12
+#define SDSS               -1
+#define LED_PIN            13
+#define FAN_PIN            9
+#define PS_ON_PIN          -1
 #define KILL_PIN           -1
-#define SUICIDE_PIN        54  //PIN that has to be turned on right after start, to keep power flowing.
+#define SUICIDE_PIN        -1  //PIN that has to be turned on right after start, to keep power flowing.
 
 #ifdef ULTRA_LCD
 
